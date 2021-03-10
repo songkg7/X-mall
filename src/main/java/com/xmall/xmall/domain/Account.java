5a4440b0add2f5933a7eed23bb7f0179144c9e8b
songkg7 <56438906+songkg7@users.noreply.github.com>
2021-03-10 16:48:42 +0900

    init: make practice branch

--- src/main/java/com/xmall/xmall/domain/Account.java
+++ src/main/java/com/xmall/xmall/domain/Account.java
@@ -1,31 +1,5 @@
 package com.xmall.xmall.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.validation.constraints.Email;
-
-@Entity
-@Getter
-@Setter
 public class Account {
-
-    @Id
-    @GeneratedValue
-    private Long id;
-
-    // Login
-    // ----
-    private String email;
-
-    private String nickname;
-
-    private String password;
-    // ----
-
-    private String Location;
 
 }
--- src/main/java/com/xmall/xmall/service/AccountService.java
+++ src/main/java/com/xmall/xmall/service/AccountService.java
@@ -1,8 +1,5 @@
 package com.xmall.xmall.service;
 
-import com.xmall.xmall.SignUpForm;
-import com.xmall.xmall.domain.Account;
-import com.xmall.xmall.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,18 +8,5 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Slf4j
 public class AccountService {
-
-    private final AccountRepository accountRepository;
-
-    public void signUp(SignUpForm signUpForm) {
-
-        Account account = new Account();
-        account.setEmail(signUpForm.getEmail());
-        account.setPassword(signUpForm.getPassword());
-
-        accountRepository.save(account);
-
-        log.info(account.getEmail());
-    }
 
 }
