bfb0b0abdc681e38c354beb20c6e15264b110393
songkg7 <56438906+songkg7@users.noreply.github.com>
2021-03-14 18:27:53 +0900

    feat: main page db 연동

--- src/main/java/com/xmall/xmall/controller/MainController.java
+++ src/main/java/com/xmall/xmall/controller/MainController.java
@@ -1,13 +1,25 @@
 package com.xmall.xmall.controller;
 
+import com.xmall.xmall.domain.Item;
+import com.xmall.xmall.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class MainController {
 
+    private final ItemRepository itemRepository;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<Item> itemLists = itemRepository.findAll();
+        model.addAttribute("itemLists", itemLists);
+
         return "main";
     }
 
