7e86fb2eaf207f6406089ff10a25cfc25e2d73c9
Kimhakseon <80378931+Kimhakseon@users.noreply.github.com>
2021-03-15 13:26:38 +0900

    view: login page error 수정 중

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,25 @@
+function checkPattern(str, regExpObj) {
+  var result = regExpObj.test(str);
+  return result;
+}
+
+function checkID(str) {
+  return checkPattern(str, new RegExp(/^[a-zA-Z0-9_@]{5,14}$/));
+}
+
+function checkPwd(str) {
+  return checkPattern(
+    str,
+    new RegExp(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^*()\-_=+\\\|\[\]{};:\'",.<>\/?]).{8,16}$/
+    )
+  );
+}
+
+function checkName(str) {
+  return checkPattern(str, new RegExp(/^[가-힣a-z]{2,8}$/));
+}
+
+function checkPhone(str) {
+  return checkPattern(str, new RegExp(/^[0-9]+$/));
+}
