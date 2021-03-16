79d7292fb86e0f69aba410733faf2bedb470801e
kimjh2715 <kimes4904@naver.com>
2021-03-16 20:14:00 +0900

    마이페이지 카테고리수정

--- src/main/java/com/xmall/xmall/controller/SettingController.java
+++ src/main/java/com/xmall/xmall/controller/SettingController.java
@@ -66,4 +66,20 @@ public class SettingController {
     @GetMapping("/as_infoguide")
     public String as_infoguide() {
         return "mypage/as_infoguide";    }
+
+    @GetMapping("/coupon")
+    public String coupon() {
+        return "mypage/coupon";    }
+
+    @GetMapping("/order_delivery")
+    public String order_delivery() {
+        return "mypage/order_delivery";    }
+
+    @GetMapping("/return_cancle")
+    public String oreturn_cancle() {
+        return "mypage/return_cancle";    }
+
+    @GetMapping("/side_mypage")
+    public String side_mypage() {
+        return "mypage/side_mypage";    }
 }
