9e18adf9cf5da43b5c7d6f8cce18d5caf6e53f37
sbkim23 <za2322@naver.com>
2021-03-16 18:23:11 +0900

    상품 주문 페이지 이동 수정 중

--- src/main/java/com/xmall/xmall/controller/SettingController.java
+++ src/main/java/com/xmall/xmall/controller/SettingController.java
@@ -1,15 +1,35 @@
 package com.xmall.xmall.controller;
 
+import com.xmall.xmall.domain.Item;
+import com.xmall.xmall.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class SettingController {
 
+    private final ItemRepository itemRepository;
+
     // FIXME: order-payment url 적절하지 않음
-    @GetMapping("/mypage/order-payment")
-    public String orderPayment() {return "mypage/order-payment";}
+    @GetMapping("/order-payment/{id}")
+    public String orderPayment(@PathVariable Long id, Model model, @RequestParam("amount") int amount) {
+        Item item = itemRepository.findById(id).get();
+
+        if (item == null) {
+            return "error";
+        }
+
+        model.addAttribute("item", item);
+        model.addAttribute("amount", amount);
+
+        return "mypage/order-payment";
+    }
 
 //    @GetMapping("/cs")
 //    public String cs() {
