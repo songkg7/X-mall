f4caa6a07a45489fc1dc5bf147bd61327e8bd184
songkg7 <56438906+songkg7@users.noreply.github.com>
2021-03-17 11:45:43 +0900

    feat: 주문결제 페이지에서 유저 정보 가져오기

--- src/main/java/com/xmall/xmall/repository/OrderRepository.java
+++ src/main/java/com/xmall/xmall/repository/OrderRepository.java
@@ -3,6 +3,5 @@ package com.xmall.xmall.repository;
 import com.xmall.xmall.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
