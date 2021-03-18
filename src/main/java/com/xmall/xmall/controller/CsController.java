cf27e286565d0865698fe2bf4d7571c780de67b2
Kimhakseon <80378931+Kimhakseon@users.noreply.github.com>
2021-03-18 11:10:40 +0900

    고객센터 이름 수정

--- src/main/java/com/xmall/xmall/controller/CsController.java
+++ src/main/java/com/xmall/xmall/controller/CsController.java
@@ -6,47 +6,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CsController {
 
-    @GetMapping("/asview")
+    @GetMapping("/cs/asview")
     public String asview() {
         return "cs/asview";
     }
 
-    @GetMapping("/cancel_refund")
+    @GetMapping("/cs/cancel_refund")
     public String cancel_refund() {
         return "cs/cancel_refund";
     }
 
-    @GetMapping("/delivery")
+    @GetMapping("/cs/delivery")
     public String delivery() {
         return "cs/delivery";
     }
 
-    @GetMapping("/member_info")
+    @GetMapping("/cs/member_info")
     public String member_info() {
         return "cs/member_info";
     }
 
-    @GetMapping("/member_service")
+    @GetMapping("/cs/member_service")
     public String member_service() {
         return "cs/member_service";
     }
 
-    @GetMapping("/notice")
+    @GetMapping("/cs/notice")
     public String notice() {
         return "cs/notice";
     }
 
-    @GetMapping("/order_file")
+    @GetMapping("/cs/order_file")
     public String order_file() {
         return "cs/order_file";
     }
 
-    @GetMapping("/refund_service")
+    @GetMapping("/cs/refund_service")
     public String refund_service() {
         return "cs/refund_service";
     }
 
-    @GetMapping("/main")
+    @GetMapping("/cs/main")
     public String main() {
         return "cs/cus_center_main";
     }
