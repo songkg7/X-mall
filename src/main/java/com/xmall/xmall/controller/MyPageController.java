18bc9a9ab7dc0aac4ef9c74eb5384fd2764e4592
cshwan23 <shchoigo0@gmail.com>
2021-03-21 02:11:50 +0900

    리뷰페이지 컨트롤러 수정중

--- src/main/java/com/xmall/xmall/controller/MyPageController.java
+++ src/main/java/com/xmall/xmall/controller/MyPageController.java
@@ -1,20 +1,24 @@
 package com.xmall.xmall.controller;
 
 import com.xmall.xmall.account.CurrentAccount;
-import com.xmall.xmall.domain.Account;
 import com.xmall.xmall.form.CheckPwdForm;
-import com.xmall.xmall.repository.MyReviewRepository;
-import com.xmall.xmall.review.My_Review;
-import com.xmall.xmall.service.MyReviewService;
-import lombok.RequiredArgsConstructor;
 import com.xmall.xmall.domain.Order;
+import com.xmall.xmall.review.ReviewCreateForm;
+import com.xmall.xmall.service.MyReviewService;
+import com.xmall.xmall.review.My_Review;
+import com.xmall.xmall.domain.Account;
 import com.xmall.xmall.repository.OrderRepository;
+import com.xmall.xmall.repository.MyReviewRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.List;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -34,12 +38,33 @@ public class MyPageController {
         return "mypage/side_mypage";
     }
 
-    @GetMapping("/my_page/side_mypage")
+    @GetMapping("/side_mypage")
     public String reviewList(Model model){
         List<My_Review> reviewList = myReviewRepository.findAll();
         model.addAttribute("reviewLists",reviewList);
         return "mypage/side_mypage";
     }
+
+
+    @GetMapping("/my_createForm")
+    public String my_createProc(@CurrentAccount Account account, Model model){
+        List<Order> orderLists = orderRepository.findByAccount(account);
+        model.addAttribute(account);
+        model.addAttribute("orderLists", orderLists);
+        model.addAttribute("form",new ReviewCreateForm());
+        return "mypage/my_createForm";
+    }
+
+
+    @PostMapping("/my_createForm")
+    public String my_createForm(@Valid ReviewCreateForm form, @CurrentAccount Account account) {
+        myReviewService.create(account, form.getSubject(),form.getMainText());
+        return "redirect:/mypage/side_mypage";
+    }
+
+
+
+
 
 
 
@@ -87,10 +112,5 @@ public class MyPageController {
         return "mypage/withdrawal";
     }
 
-    // 최근주문내역 -> 리뷰 작성
-    @GetMapping("/review")
-    public String review(@CurrentAccount Account account, Model model) {
-        model.addAttribute(account);
-        return "mypage/review";
-    }
+
 }
