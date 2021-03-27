e293f7b7b6beb9c356644b68d30e5c92224affc3
songkg7 <56438906+songkg7@users.noreply.github.com>
2021-03-27 11:51:02 +0900

    fix: security config csrf token 활성화

--- src/main/java/com/xmall/xmall/config/SecurityConfig.java
+++ src/main/java/com/xmall/xmall/config/SecurityConfig.java
@@ -32,10 +32,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/myPage/**", "/order/**").authenticated()
                 .anyRequest().permitAll();
 
-        // FIXME: csrf token 체크 기능 비활성화, 데이터베이스 공격에 취약해질 수 있으므로 반드시 다시 활성화해야한다.
-        // -- form tag 에 csrf token 값이 제대로 생성되지 않고 있는 것 같으니 체크해보자.
-        http.csrf().disable();
-
         http.sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
 //         login
