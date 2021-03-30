2f05677b69cc51529d93c3ff7ad92852f652a7bc
songkg7 <song_kg@naver.com>
2021-03-30 20:43:24 +0900

    fix: query 수정 중

--- src/main/java/com/xmall/xmall/repository/AccountRepositoryExtensionImpl.java
+++ src/main/java/com/xmall/xmall/repository/AccountRepositoryExtensionImpl.java
@@ -1,22 +1,22 @@
-package com.xmall.xmall.repository;
-
-import com.xmall.xmall.domain.Account;
-import com.xmall.xmall.domain.QAccount;
-import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
-
-import java.util.List;
-
-import static com.xmall.xmall.domain.QAccount.*;
-
-public class AccountRepositoryExtensionImpl extends QuerydslRepositorySupport implements AccountRepositoryExtension {
-
-    public AccountRepositoryExtensionImpl() {
-        super(Account.class);
-    }
-
-    @Override
-    public List<Account> findAccountPerDay() {
-        from(account)
-                .where(account.JoinedAt)
-    }
-}
+//package com.xmall.xmall.repository;
+//
+//import com.xmall.xmall.domain.Account;
+//import com.xmall.xmall.domain.QAccount;
+//import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+//
+//import java.util.List;
+//
+//import static com.xmall.xmall.domain.QAccount.*;
+//
+//public class AccountRepositoryExtensionImpl extends QuerydslRepositorySupport implements AccountRepositoryExtension {
+//
+//    public AccountRepositoryExtensionImpl() {
+//        super(Account.class);
+//    }
+//
+//    @Override
+//    public List<Account> findAccountPerDay() {
+//        from(account)
+//                .where(account.JoinedAt)
+//    }
+//}
